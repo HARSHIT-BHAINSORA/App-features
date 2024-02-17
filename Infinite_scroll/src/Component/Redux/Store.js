11733afb2e0bd07bd878db4fc29e3a32c9065507
Harshit <bharshit468@gmail.com>
2024-02-17 13:18:25 +0530

    Redux Cart Featrue

--- Infinite_scroll/src/Component/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { cartReducer } from "./Reducer";
-
-export const Store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
